34f992ec07a614bb2d7816ad557b31b4cac2c5fc
peltzefa <peltzefa@gmail.com>
2016-03-22 10:13:17 +0100

    Velocity

--- FlightRadar/FlightRadar.Model/ADSBVelocityMessage.cs
+++ FlightRadar/FlightRadar.Model/ADSBVelocityMessage.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FlightRadar.Model
 {
-    class ADSBVelocityMessage : ADSBMessageBase
+    public class ADSBVelocityMessage : ADSBMessageBase
     {
 
     }
