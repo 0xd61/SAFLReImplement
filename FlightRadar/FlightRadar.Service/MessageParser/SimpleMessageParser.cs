43868414980f3a4e98d145397613eb1b0d1b9fe9
BattleCake451 <johannes.steinhuelb@web.de>
2016-03-22 10:33:04 +0100

    Aufräum arbeiten - MessageContainer

---
+++ FlightRadar/FlightRadar.Service/MessageParser/SimpleMessageParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FlightRadar.Service.MessageParser
+{
+    class SimpleMessageParser : IMessageParser
+    {
+        /// <summary>
+        /// parse String from JSON ADSB Sentence
+        /// </summary>
+        /// <param name="adsbSentence"></param>
+        /// <returns></returns>
+        public string Parse(string adsbSentence)
+        {
+            Regex pattern = new Regex(@"\d+\.\d+!ADS-B\*[0-9A-Z]{28}");
+            string myString = pattern.Match(adsbSentence).ToString();
+            return myString;
+        }
+    }
+}
