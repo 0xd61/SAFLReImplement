39bedbc28eb3cb48ad82349a3585228629e58532
peltzefa <peltzefa@gmail.com>
2016-03-22 10:36:17 +0100

    MessageBase

--- FlightRadar/FlightRadar.Model/ADSBMessageBase.cs
+++ FlightRadar/FlightRadar.Model/ADSBMessageBase.cs
@@ -8,7 +8,21 @@ namespace FlightRadar.Model
 {
     public class ADSBMessageBase
     {
-        public string Name { get; set; } = string.Empty;
-        public string ICAO { get; set; } = string.Empty;
+        private string timestamp { get; set; } = string.Empty;
+        private string ICAO { get; set; } = string.Empty;
+        private int type { get; set; }
+        private int downlinkFormat { get; set; }
+        private int capability { get; set; }
+        private int payload { get; set; }
+
+        public ADSBMessageBase (string timestamp, string ICAO, int type, int downlinkFormat, int capability, int payload)
+        {
+            this.timestamp = timestamp;
+            this.ICAO = ICAO;
+            this.type = type;
+            this.downlinkFormat = downlinkFormat;
+            this.capability = capability;
+            this.payload = payload;
+        }
     }
 }
