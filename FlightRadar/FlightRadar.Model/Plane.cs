407259750167312e7e0debcaa301819b8bd82a49
peltzefa <peltzefa@gmail.com>
2016-03-29 10:17:38 +0200

    Plane

---
+++ FlightRadar/FlightRadar.Model/Plane.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightRadar.Model
+{
+    class Plane
+    {
+        public string ICAO { get; set; } = string.Empty;
+        public ADSBMessageContainer Messages = new ADSBMessageContainer();
+    }
+}
