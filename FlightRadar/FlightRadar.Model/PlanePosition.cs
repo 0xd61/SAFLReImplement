5044e491fe8026dbb06e7d54045dd8b03036e397
peltzefa <peltzefa@gmail.com>
2016-03-29 11:59:13 +0200

    Position in Model & CPR Coding angefangen

---
+++ FlightRadar/FlightRadar.Model/PlanePosition.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightRadar.Model
+{
+    class PlanePosition
+    {
+        public DateTime PositionTimestamp { get; }
+        public double Latitude { get; }
+        public double Longitude { get; }
+        public double Altitude { get; }
+
+        public PlanePosition(DateTime Timestamp, double Latitude, double Longitude, double Altitude)
+        {
+            this.PositionTimestamp = Timestamp;
+            this.Latitude = Latitude;
+            this.Longitude = Longitude;
+            this.Altitude = Altitude;
+        }
+    }
+}
