f90482e1951e588e81085ea8e1c4a1ac956ef2fa
peltzefa <peltzefa@gmail.com>
2016-04-05 14:56:03 +0200

    Fuck Yäh bimbumbiiiiiii SimpleIdentificationParser is rädiiiiiiii!

--- FlightRadar/FlightRadar.Service/MessageParser/SimpleIdentificationParser.cs
+++ FlightRadar/FlightRadar.Service/MessageParser/SimpleIdentificationParser.cs
@@ -9,18 +9,39 @@ namespace FlightRadar.Service.MessageParser
 {
     public class SimpleIdentificationParser : IPayloadParser
     {
-        public ADSBMessageBase ParseMessage(ADSBMessageBase message)
+
+        public char[] SixBitChar = { '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?' };
+
+        public ADSBMessageBase ParseMessage(ADSBMessageBase Message)
         {
-            ADSBIdentificationMessage tmpMessage = (ADSBIdentificationMessage)message;
+            ADSBIdentificationMessage TmpMessage = (ADSBIdentificationMessage)Message;
 
+            TmpMessage.EmitterCategory = ParseEmmiterCategory(Message.Payload);
+            TmpMessage.AircraftID = ParseAircraftID(Message.Payload);
 
-
-            return tmpMessage;
+            return TmpMessage;
         }
 
-        private int ParseAltitude(string payloadInBin)
+        private int ParseEmmiterCategory(String payload)
         {
-
+            return Convert.ToInt32(payload.Substring(5, 3), 2);
         }
+
+        private String ParseAircraftID(String payload)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(SixBitChar[Convert.ToInt32(payload.Substring(8, 6), 2)]);
+            sb.Append(SixBitChar[Convert.ToInt32(payload.Substring(14, 6), 2)]);
+            sb.Append(SixBitChar[Convert.ToInt32(payload.Substring(20, 6), 2)]);
+            sb.Append(SixBitChar[Convert.ToInt32(payload.Substring(26, 6), 2)]);
+            sb.Append(SixBitChar[Convert.ToInt32(payload.Substring(32, 6), 2)]);
+            sb.Append(SixBitChar[Convert.ToInt32(payload.Substring(38, 6), 2)]);
+            sb.Append(SixBitChar[Convert.ToInt32(payload.Substring(44, 6), 2)]);
+            sb.Append(SixBitChar[Convert.ToInt32(payload.Substring(50, 6), 2)]);
+
+            return sb.ToString();
+        }
+
+
     }
 }
