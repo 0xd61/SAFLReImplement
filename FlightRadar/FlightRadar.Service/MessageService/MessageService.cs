383d28265c6805864bca658cbfb44ddc03e5cf93
BattleCake451 <johannes.steinhuelb@web.de>
2016-04-19 10:27:15 +0200

    MessageService

--- FlightRadar/FlightRadar.Service/MessageService/MessageService.cs
+++ FlightRadar/FlightRadar.Service/MessageService/MessageService.cs
@@ -21,12 +21,28 @@ namespace FlightRadar.Service
         public MessageService(IMessageRepository repository)
         {
             repo = repository;
-            repo.OnGetMessage += Repo_OnGetMessage;
+
+            AddEventHandler();
+            StartMessageThread();
+        }
+
+        /// <summary>
+        /// Starts a separate thread to fetch the messages from the server.
+        /// </summary>
+        private void StartMessageThread()
+        {
 
             workerThread = new Thread(repo.StartMessageLoop);
             workerThread.Name = "Message Thread";
             workerThread.Start();
+        }
 
+        /// <summary>
+        /// Adds all necessary event handler.
+        /// </summary>
+        private void AddEventHandler()
+        {
+            repo.OnGetMessage += Repo_OnGetMessage;
         }
 
         /// <summary>
@@ -36,7 +52,6 @@ namespace FlightRadar.Service
         private void Repo_OnGetMessage(string message)
         {
             rawMessages.Enqueue(message);
-            //Console.WriteLine(message);
         }
 
         /// <summary>
@@ -49,7 +64,6 @@ namespace FlightRadar.Service
             {
                 Console.WriteLine("Keine Verbindung zum Repository...");
             }
-
 
             if (rawMessages.Count != 0)
                 return rawMessages.Dequeue();
