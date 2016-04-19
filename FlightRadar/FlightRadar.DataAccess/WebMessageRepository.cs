4fc7b5b68e0e080a789cee68dc9db4370b01fffb
peltzefa <peltzefa@gmail.com>
2016-04-19 10:10:36 +0200

    Commented WebMessageRepository

--- FlightRadar/FlightRadar.DataAccess/WebMessageRepository.cs
+++ FlightRadar/FlightRadar.DataAccess/WebMessageRepository.cs
@@ -10,6 +10,9 @@ using System.Threading;
 
 namespace FlightRadar.DataAccess
 {
+    /// <summary>
+    /// tries to connect to the server and tries to read the datastream
+    /// </summary>
     public class WebMessageRepository : IMessageRepository
     {
         public string ServerURL { get; private set; } = string.Empty;
@@ -50,15 +53,20 @@ namespace FlightRadar.DataAccess
             }
         }
 
+        /// <summary>
+        /// connects to the server
+        /// </summary>
         private void Connect()
         {
             request = WebRequest.Create(ServerURL);
-
 
             ((HttpWebRequest)request).UserAgent = ".NET Framework Example Client";
             response = request.GetResponse();
         }
 
+        /// <summary>
+        /// closes the connection
+        /// </summary>
         private void CloseConnection()
         {
             reader?.Close();
@@ -67,6 +75,9 @@ namespace FlightRadar.DataAccess
             Console.WriteLine("Verbindung geschlossen...");
         }
 
+        /// <summary>
+        /// reads messages from messagestream and invokes an event 
+        /// </summary>
         private void MessageLoop()
         {
             char[] buffer = new char[100];
@@ -79,17 +90,32 @@ namespace FlightRadar.DataAccess
             }
         }
 
+        /// <summary>
+        /// reads string from buffer
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
         private string ReadMessageStream(char[] buffer)
         {
             reader.Read(buffer, 0, 100);
             return new string(buffer);
         }
 
+        /// <summary>
+        /// validates message
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         private bool IsValidMessage(string message)
         {
             return message.Contains("ADS-B") && message != string.Empty;
         }
 
+
+        /// <summary>
+        /// notifies eventlistener when messageevent is created
+        /// </summary>
+        /// <param name="message"></param>
         private void RaiseEventMessageReceived(string message)
         {
             if (IsValidMessage(message))
